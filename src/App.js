1d45300fd022555449303d42c61641fb20147aa1
kamal aryal <mraryalkamal2011@gmail.com>
2021-12-04 23:31:38 +0545

    navbar completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/layout/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+      </div>
+    )
+  }
+}
+
+export default App
